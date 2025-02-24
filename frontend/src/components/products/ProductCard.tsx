cbc5c005a33d0b138923bb58d908abf72e5af6ed
GaspardPauchet <gaspardpauchet@gmail.com>
2025-02-24 11:46:19 +0100

    add merch img

--- frontend/src/components/products/ProductCard.tsx
+++ frontend/src/components/products/ProductCard.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface ProductCardProps {
   title: string;
   price: number;
-}
+  product: {
+    name: string;
+    price: number;
+    image: string;
+  };
+} 
 
 const ProductCard = ({ title, price }: ProductCardProps) => {
   return (
     <div className="bg-white rounded-xl shadow-sm hover:shadow-md transition-all duration-300 overflow-hidden group">
-      <div className="h-48 bg-gray-100 group-hover:bg-gray-200 transition-colors"></div>
+   <img src="./merch/pull.png" alt="pull" />
       <div className="p-4">
         <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
         <p className="text-[#E6AACE] font-medium my-2">{price.toFixed(2)} €</p>
